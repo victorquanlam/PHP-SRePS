298d8c1800d04c978b1b3b0f3d92c22b2ac93b36
QuanLam <37169648+hiendaica11145@users.noreply.github.com>
2018-09-06 18:23:58 +1000

    fix the index

--- php/index.php
+++ php/index.php
@@ -1,60 +1,15 @@
-<?php 
-require_once 'php_action/db_connect.php';
+<?php
+//check connection to database
+require_once("C:/xampp/htdocs/sale_system/php/dp_connect.php")
 
-session_start();
 
-if(isset($_SESSION['userId'])) {
-	header('location: http://localhost:9080/sale/dashboard.php');	
-}
-
-$errors = array();
-
-if($_POST) {		
-
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-
-	if(empty($username) || empty($password)) {
-		if($username == "") {
-			$errors[] = "Username is required";
-		} 
-
-		if($password == "") {
-			$errors[] = "Password is required";
-		}
-	} else {
-		$sql = "SELECT * FROM users WHERE username = '$username'";
-		$result = $connect->query($sql);
-
-		if($result->num_rows == 1) {
-			$password = md5($password);
-			// exists
-			$mainSql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-			$mainResult = $connect->query($mainSql);
-
-			if($mainResult->num_rows == 1) {
-				$value = $mainResult->fetch_assoc();
-				$user_id = $value['user_id'];
-
-				// set session
-				$_SESSION['userId'] = $user_id;
-
-				header('location: http://localhost:9080/sale/dashboard.php');	
-			} else{
-				
-				$errors[] = "Incorrect username/password combination";
-			} // /else
-		} else {		
-			$errors[] = "Username doesnot exists";		
-		} // /else
-	} // /else not empty username // password
-	
-} // /if $_POST
 ?>
 
 <!DOCTYPE html>
 <html>
-<title>Sale Record System</title>
+<head>
+
+	<title>Sale Record System</title>
 	<!--Bootstrap-->
 	<link rel="stylesheet" type="text/css" href="../assets/bootstrap/css/bootstrap.min.css">
 
@@ -71,80 +26,13 @@ if($_POST) {
 	<script type="text/javascript" src="../assets/jqueryiu/jquery-ui.min.js"></script>
 	<!--bootstrap js-->
 	<script type="text/javascript" src="../assets/bootstrap/js/bootstrap.min.js"></script>
-<body>
-	<div class="container">
-		<div class="row vertical">
-			<div class="col-md-5 col-md-offset-4">
-				<div class="panel panel-info">
-					<div class="panel-heading">
-						<h3 class="panel-title">Please Sign in</h3>
-					</div>
-					<div class="panel-body">
-
-						<div class="messages">
-							<?php if($errors) {
-								foreach ($errors as $key => $value) {
-									echo '<div class="alert alert-warning" role="alert">
-									<i class="glyphicon glyphicon-exclamation-sign"></i>
-									'.$value.'</div>';										
-									}
-								} ?>
-						</div>
-
-						<form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="loginForm">
-							<fieldset>
-							  <div class="form-group">
-									<label for="username" class="col-sm-2 control-label">Username</label>
-									<div class="col-sm-10">
-									  <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="off" />
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="password" class="col-sm-2 control-label">Password</label>
-									<div class="col-sm-10">
-									  <input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off" />
-									</div>
-								</div>								
-								<div class="form-group">
-									<div class="col-sm-offset-2 col-sm-10">
-									  <button type="submit" class="btn btn-default"> <i class="glyphicon glyphicon-log-in"></i> Sign in</button>
-									</div>
-								</div>
-							</fieldset>
-						</form>
-					</div>
-					<!-- panel-body -->
-				</div>
-				<!-- /panel -->
-			</div>
-			<!-- /col-md-4 -->
-		</div>
-		<!-- /row -->
-	</div>
-	<!-- container -->	
-</body>
-</html>
-
-
-
-
-
-
-
-	
-
-<!DOCTYPE html>
-<html>
-<head>
-
-	
 </head>
 
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-5">
-				Test
+				testing. Great you are on the right track 
 			</div>
 		</div>
 	</div>

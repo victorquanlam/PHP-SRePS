9eb3c71702c31c4c0776ed6ee1d99fd4034a36c8
KD_W <43028287+Kadyw@users.noreply.github.com>
2018-09-18 16:48:34 +1000

    Display Sales and Edit Sales
    I edited the sales.php filed, custom.css files and added edit.php file. Display sales adn edit sales now works.

---
+++ edit.php
@@ -0,0 +1,73 @@
+<?php
+require_once("action/dp_connect.php");
+require_once("includes/header.php");
+$id = $_GET['id'];
+$sql = "SELECT * FROM `sale` WHERE `sale_id` = $id";
+$result = $connect->query($sql);
+$row = $result->fetch_assoc();
+?>
+<!--put your content hereeeeeeeeeeeeeee-->
+<html>
+<head>
+<link rel="stylesheet" type="text/css" src="custom/css/custom.css" />
+</head>
+
+<body>
+  <div class ="edit_sale_box">
+  <h2><span class="fontawesome-lock"></span>Edit Sale</h2>
+  <form method="post"  action="sales.php">
+    <fieldset>
+    <div>
+      <label for="sale_id" class="edit_label">Item ID:</label>
+      <input type="text" class="edit_input" value = <?php echo $row["sale_id"]; ?> onfocus="this.blur()" name="sale_id" id="sale_id" readonly>
+    </div>
+    <div>
+      <label for="sale_date" class="edit_label">Date:</label>
+      <input type="text" class="edit_input" value = <?php echo $row["sale_date"]; ?> onfocus="this.blur()" name="sale_date" id="sale_date" readonly>
+    </div>
+    <div>
+      <label for="sale_name" class="edit_label">Name:</label>
+      <input type="text" class="edit_input" value = <?php echo $row["sale_name"]; ?> onfocus="this.blur()" name="sale_name" id="sale_name" readonly>
+    </div>
+    <div>
+      <label for="total_amount" class="edit_label">Amount:</label>
+      <input type="text" class="edit_input" value = <?php echo $row["total_amount"]; ?> name="total_amount" id="total_amount">
+    </div>
+    <div>
+      <label for="discount" class="edit_label">Discount:</label>
+      <input type="text" class="edit_input" value = <?php echo $row["discount"]; ?> name="discount" id="discount">
+    </div>
+    <div>
+      <label for="payment_type" class="edit_label">Payment Type:</label>
+      <select name = "payment_type">
+        <option value="Credit Card" <?php if($row["payment_type"]=="Credit Card") echo"selected='selected'"?>>Credit Card</option>
+        <option value="Eftpos"<?php if($row["payment_type"]=="Eftpos") echo"selected='selected'"?>>Eftpos</option>
+        <option value="Cash"<?php if($row["payment_type"]=="Cash") echo"selected='selected'"?>>Cash</option>
+      </select>
+    </div>
+    <div>
+      <label for="payment_status" class="edit_label">Payment Status:</label>
+      <select name = "payment_status">
+        <option value="Completed" <?php if($row["payment_status"]=="Completed") echo"selected='selected'"?>>Completed</option>
+        <option value="Waiting"<?php if($row["payment_status"]=="Waiting") echo"selected='selected'"?>>Waiting</option>
+      </select>
+    </div>
+    <div>
+      <label for="product_price" class="edit_label">Price:</label>
+      <input type="text" class="edit_input" value = <?php echo $row["product_price"]; ?> name="product_price" id="product_price">
+    </div>
+    <div class="edit_button">
+      <input type="submit" class="button_primary" id="submit" value="Submit" />
+  	  <input type="reset" class="button_primary"  id="cancel" value="Cancel" />
+    </div>
+  </fieldset>
+  </form>
+  </div>
+</body>
+
+</html>
+
+
+<?php
+
+require_once("includes/footer.php"); ?>
--- sales.php
+++ sales.php
@@ -1,16 +1,52 @@
-<?php 
+<?php
 require_once("action/dp_connect.php");
 require_once("includes/header.php");
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  $id = $_POST["sale_id"];
+  $total_amount = $_POST["total_amount"];
+  $discount = $_POST["discount"];
+  $payment_type = $_POST["payment_type"];
+  $payment_status = $_POST["payment_status"];
+  $price = $_POST["product_price"];
+  mysqli_query($connect, "UPDATE sale SET total_amount = '$total_amount', discount ='$discount', payment_type = '$payment_type', payment_status = '$payment_status', product_price='$price' WHERE sale_id='$id'");
+}
+$sql = "SELECT * FROM `sale`";
+$result = $connect->query($sql);
+echo "<h2>Sales Records</h2><hr \>";
+echo"<table border='1em' class='table table-bordered table-striped table-hover'>";
+echo"
+<tr>
+  <td>Sale ID</td>
+  <td>Date</td>
+  <td>Name</td>
+  <td>Amount</td>
+  <td>Discount</td>
+  <td>Payment Type</td>
+  <td>Status</td>
+  <td>Price</td>
+  <td>Action</td>
+</tr>";
+while($row = $result->fetch_assoc())
+{
+    echo"
+    <tr>
+        <td>".$row["sale_id"]."</td>
+        <td>".$row["sale_date"]."</td>
+        <td>".$row["sale_name"]."</td>
+        <td>".$row["total_amount"]."</td>
+        <td>".$row["discount"]."</td>
+        <td>".$row["payment_type"]."</td>
+        <td>".$row["payment_status"]."</td>
+        <td>".$row["product_price"]."</td>
+        <td><a href='edit.php?id=".$row["sale_id"]."' target = '_blank'/a>Edit</td>
+    </tr>";
+}
+echo"</table>";
 ?>
 
 <!--put your content hereeeeeeeeeeeeeee-->
 
-
-
-
-
-
-<?php 
+<?php
 
 require_once("includes/footer.php"); ?>
-
